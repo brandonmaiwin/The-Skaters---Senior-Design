28314974d599e8c0f60ede82eb2378d7f1d475c6
Brandon Mai Nguyen <BrandonMaiNguyen@brandons-air.home>
2018-10-05 19:37:30 -0700

    The following code shows the slider and speed display functionality.

--- Bluetooth connection test/ViewController.swift
+++ Bluetooth connection test/ViewController.swift
@@ -9,13 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    
+    @IBOutlet weak var speedometer: UILabel!
+    @IBAction func speedslider(_ sender: UISlider)
+    {
+            speedometer.text = String(sender.value)
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
